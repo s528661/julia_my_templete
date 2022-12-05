1a93814999ebf1864a7946b4d5f7f27f9b7f717a
citrus1998 <ID+username@users.noreply.github.com>
2022-12-05 19:01:19 +0900

    20221205

---
+++ templete.jl
@@ -0,0 +1,38 @@
+import Pkg
+Pkg.add("ArgParse")
+using ArgParse
+
+function parse_commandline(p_settings)
+    @add_arg_table p_settings begin
+        "π"
+            help = "Pi"
+            # arg_type = Irrational{:π}
+            default = Float64(pi)
+        "e"
+            help = "Exp"
+            # arg_type = Irrational{:ℯ}
+            default = Float64(ℯ)
+        "i"
+            help = "Imaginary number"
+            # arg_type = Complex{Int64}
+            default = 1im
+    end
+
+    return parse_args(p_settings)
+end
+
+function main()
+    s = ArgParseSettings()
+    args = parse_commandline(s)
+    
+    println("Parsed args:")
+    for (key, value) in args
+        println("  $key  =>  $value")
+    end
+
+    println("Euler's formula: e^(πi) = ", args["e"]^(args["π"]*args["i"]))
+end
+ 
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
